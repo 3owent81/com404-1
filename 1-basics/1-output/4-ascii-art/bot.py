d5bfb8da26fe563a9d5f28031a07cdc5ccd691d4
brucebonus <43637138+brucebonus@users.noreply.github.com>
2018-09-27 11:02:58 +0100

    Create 4-ascii-art/bot.py
    Added code to display ascii art

---
+++ 1-basics/1-output/4-ascii-art/bot.py
@@ -0,0 +1,9 @@
+# Display ascii art
+print("\t ############## ")
+print("\t(#  x     x   #)")
+print("\t #            #")
+print("\t #     w      #")
+print("\t ##############")
+print("\t |            |")
+print("\t |  #        #|")
+print("\t |   #        |")
