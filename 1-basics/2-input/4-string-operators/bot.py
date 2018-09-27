05ad55d646034d3aad1daad09ffcb4456b80da4f
brucebonus <43637138+brucebonus@users.noreply.github.com>
2018-09-27 12:44:50 +0100

    Create 4-string-operators/bot.py
    Added code to demonstrate the effect of the multiply operator on strings

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,12 @@
+print("please enter the number of lives remaining:")
+lives = int(input())
+
+print("please enter the energy level:")
+energy = int(input())
+
+print("please enter the shield level:")
+shield = int(input())
+
+print("Lives:\t", "♥" * lives)
+print("Energy:\t", "♦" * energy)
+print("Shield:\t", "♦" * shield)
