0f4ce899bb2c391e473f85f38ba41f556f009619
brucebonus <43637138+brucebonus@users.noreply.github.com>
2018-09-27 12:07:23 +0100

    Create 2-ascii-robot/bot.py
    Added code to display a dynamic ascii art

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,13 @@
+# Read user input
+print("Please enter eye character")
+eye = input()
+
+print("Your eye is ", eye, ".", sep="")
+print("\t ############## ")
+print("\t(#  ",eye," ",eye,"#)")
+print("\t #            #")
+print("\t #     w      #")
+print("\t ##############")
+print("\t |            |")
+print("\t |  #        #|")
+print("\t |   #        |")
