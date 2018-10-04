117ca502086c3a36ee141bffda0ff80f0b1b362c
brucebonus <43637138+brucebonus@users.noreply.github.com>
2018-10-04 10:31:51 +0100

    Create bot.py

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,11 @@
+# Read in directions from user
+print(" Please enter a direction from the following:")
+print("...w to move up")
+print("...a to move left")
+print("...s to move down")
+print("...d to move right")
+
+direction = input()
+
+if (direction == "w"):
+  print("I am moving up!")
