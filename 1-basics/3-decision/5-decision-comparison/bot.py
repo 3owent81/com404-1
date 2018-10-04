c7fcef3f5d8b1d0b16c5dbe1275ac263b77eac42
brucebonus <43637138+brucebonus@users.noreply.github.com>
2018-10-04 10:54:03 +0100

    Create bot.py
    Added code to demonstrate the use of 'if' to work out which is the greater number

---
+++ 1-basics/3-decision/5-decision-comparison/bot.py
@@ -0,0 +1,11 @@
+print("Please enter first number")
+first_number = float(input())
+
+print("Please enter second number")
+second_number = float(input())
+
+# Work out which number is the largest
+if (first_number > second_number):
+  print("First number is greater")
+elif (first_number < second_number):
+  print("Second number is greater")
