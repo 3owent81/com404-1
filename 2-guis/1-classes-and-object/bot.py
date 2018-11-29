b10be10d5ff4586cc548ec14b95b628c4aa6bed3
brucebonus <43637138+brucebonus@users.noreply.github.com>
2018-11-29 12:16:37 +0000

    Update bot.py
    Made code to demonstrate classes and object

--- 2-guis/1-classes-and-object/bot.py
+++ 2-guis/1-classes-and-object/bot.py
@@ -1,31 +1,31 @@
-class Person:
-    def __init__(self, name, age=0, energy=0, shield=0):
-        self.name = name
-        self.age = age
-        self.energy = energy
-        self.shield = shield
-        
-    def display_name(self):
-        print("I am {}.".format(self.name))
+class Bot:
+  def __init__(self, name, energy, shield):
+    self.name = name
+    self.age = 0
+    self.energy = energy 
+    self.shield = shield
+
+  def display_name(self):
+    print(self.name)
+
+  def display_age(self):
+    print(self.age)
+
+  def display_energy(self):
+    print(self.energy)
+
+  def display_shield(self):
+    print(self.shield)
+
+  def __str__(self):
+        print("{} is {} years old and the energy level is {} and shield level is {}".format(self.name, self.age, self.energy, self.shield))
     
-    def display_age(self):
-        print("I am {} years old.".format(self.age))
+# object
+beep = Bot("Beep", 100, 100)
 
-    def display_energy(self):
-        print("My energy level is {}%".format(self.energy))
+beep.display_name()
+beep.display_age()
+beep.display_energy()
+beep.display_shield()
 
-    def display_shield(self):
-        print("My shield level is {}%".format(self.shield))
-
-    def display_summary(self):
-        print("I am {}. I am {} years old. My energy level is {}% and shield is {}%".format(self.name, self.age, self.energy, self.shield))
-
-    def __str__(self):
-        return
-
-bot = Person("bot", 34, 85, 50)
-bot.display_name()
-bot.display_age()
-bot.display_energy()
-bot.display_shield()
-bot.display_summary() 
+beep.__str__()
